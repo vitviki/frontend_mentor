8c24e450aa7ebb79f88b2b9e08c0a0ceb27d60b8
vitviki <vtyagi1991@gmail.com>
2024-06-23 18:39:29 +0530

    added colors, font and animations

--- countries/tailwind.config.js
+++ countries/tailwind.config.js
@@ -2,7 +2,54 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "hsl(209, 23%, 22%)",
+        veryDarkBlueDarkMode: "hsl(207, 26%, 17%)",
+        veryDarkBlueLightMode: "hsl(200, 15%, 8%)",
+        darkGray: "hsl(0, 0%, 52%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        nunito: ["Nunito Sans", "sans-serif"],
+      },
+      animation: {
+        slideup: "slideup 1s ease-in-out",
+        slidedown: "slidedown 1s ease-in-out",
+        slideleft: "slideleft 1s ease-in-out",
+        slideright: "slideright 1s ease-in-out",
+        wave: "wave 1.2s linear infinite",
+        slowfade: "slowfade 2.2s ease-in-out",
+      },
+      keyframes: {
+        slowfade: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        slideup: {
+          from: { opacity: 0, transform: "translateY(25%)" },
+          to: { opacity: 1, transform: "none" },
+        },
+        slidedown: {
+          from: { opacity: 0, transform: "translateY(-25%)" },
+          to: { opacity: 1, tranform: "none" },
+        },
+        slideleft: {
+          from: { opacity: 0, tranform: "translateX(-20px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
+        },
+        slideright: {
+          from: { opacity: 0, transform: "translateX(20px)" },
+          to: { opacity: 1, tranform: "translateX(0)" },
+        },
+        wave: {
+          "0%": { transform: "scale(0)" },
+          "50%": { transform: "scale(1)" },
+          "100%": { transform: "scale(0)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
