96857f16a358fde11deb2000f3a3eddd1d3ef256
vitviki <vtyagi1991@gmail.com>
2024-06-25 16:01:09 +0530

    added 'filter by region' functionality

--- countries/src/pages/Home.jsx
+++ countries/src/pages/Home.jsx
@@ -3,9 +3,11 @@ import { useSelector } from "react-redux";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 import { MdSearch } from "react-icons/md";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 import {
   useGetAllCountriesQuery,
   useGetSearchResultsQuery,
+  useGetCountriesByRegionQuery,
 } from "../redux/services/restCountriesCore";
 import CountryCard from "../components/CountryCard";
 import { regions } from "../constants/constants";
@@ -15,18 +17,31 @@ const Home = () => {
   const { mode } = useSelector((store) => store.mode);
   const [region, setRegion] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+  const [isDropdownOpen, setIsDropDownOpen] = useState(false);
 
+  function updateRegion(value) {
+    setIsDropDownOpen(false);
+    setRegion(value);
+    setSearchTerm("");
+  }
+
+  // Get all countries data
   const {
     data: countriesData,
     isFetching: fetchingCountriesData,
     error: countriesError,
   } = useGetAllCountriesQuery();
 
+  // Search for a country by name
   const {
     data: searchResult,
     isFetching: fetchingSearchResults,
     error: searchError,
   } = useGetSearchResultsQuery(searchTerm);
+
+  // Get all countries by region
+  const { data: regionResult, isFetching: fetchingRegionResult } =
+    useGetCountriesByRegionQuery(region);
 
   if (fetchingCountriesData) {
     return <Loader />;
@@ -61,9 +76,49 @@ const Home = () => {
             }`}
           />
         </div>
+        <div
+          className={`relative w-[10%] py-3 px-3 ${
+            mode === "light"
+              ? "text-veryDarkBlueLightMode bg-white"
+              : "text-white bg-darkBlue"
+          }  rounded-lg shadow-md`}
+        >
+          <button
+            onClick={() => setIsDropDownOpen((prev) => !prev)}
+            className="w-full flex items-center justify-around"
+          >
+            {region === ""
+              ? "Filter by region"
+              : region.charAt(0).toUpperCase() + region.slice(1)}
+            {isDropdownOpen ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+          </button>
+          {isDropdownOpen && (
+            <div
+              className={`w-full absolute ${
+                mode === "light"
+                  ? "bg-white text-veryDarkBlueLightMode"
+                  : "bg-darkBlue text-white"
+              }  top-14 left-0 p-2 rounded-lg shadow-md`}
+            >
+              {regions.map((r) => (
+                <div
+                  className={`w-full cursor-pointer py-1 pl-1 ${
+                    mode === "light"
+                      ? "hover:bg-veryLightGray"
+                      : "hover:bg-veryDarkBlueDarkMode"
+                  } `}
+                  key={r.value}
+                  onClick={() => updateRegion(r.value)}
+                >
+                  {r.title}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
       {searchResult !== undefined && searchTerm.length > 0 ? (
-        <div className="w-full flex flex-wrap justify-between gap-12 overflow-y-clip">
+        <div className="w-full grid grid-cols-6 gap-12">
           {searchResult?.map((country, idx) => (
             <Link
               to={`/${country.name.official}`}
@@ -74,8 +129,20 @@ const Home = () => {
             </Link>
           ))}
         </div>
+      ) : regionResult !== undefined ? (
+        <div className="w-full grid grid-cols-6 gap-12">
+          {regionResult?.map((country, idx) => (
+            <Link
+              to={`/${country.name.official}`}
+              title={country.name.official}
+              key={idx}
+            >
+              <CountryCard country={country} />
+            </Link>
+          ))}
+        </div>
       ) : (
-        <div className="w-full flex flex-wrap justify-between gap-12 overflow-y-clip">
+        <div className="w-full grid grid-cols-6 gap-12">
           {countriesData?.slice(0, 30).map((country, idx) => (
             <Link
               to={`/${country.name.official}`}
