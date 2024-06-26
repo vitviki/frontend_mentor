237e00008b7ab3ff6f8178d86d96e62fffa61890
vitviki <vtyagi1991@gmail.com>
2024-06-26 11:26:37 +0530

    created button element

--- countries/src/components/Button.jsx
+++ countries/src/components/Button.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ icon, text, to }) => {
+  const { mode } = useSelector((store) => store.mode);
+  return (
+    <div>
+      <Link
+        to={to}
+        className={`w-[120px] flex items-center justify-around py-2 px-5 shadow-lg ${
+          mode === "light"
+            ? "bg-white text-veryDarkBlueLightMode"
+            : "bg-darkBlue text-white"
+        } rounded-lg`}
+      >
+        {icon}
+        {text}
+      </Link>
+    </div>
+  );
 };
 
 export default Button;
